9a8f1f4c9a4e370cf69adc37ac24569ddc53833a
dliuproduction <dliuproduction@gmail.com>
2018-05-20 21:55:14 -0400

    DAO contract finished, all functions working

--- contracts/Authentication.sol
+++ contracts/Authentication.sol
@@ -5,7 +5,7 @@ import '../node_modules/openzeppelin-solidity/contracts/lifecycle/Destructible.s
 import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Authentication is Destructible {
-using SafeMath for uint;
+using SafeMath for uint256;
 
   // type for a single member
   struct Member {
@@ -13,43 +13,44 @@ using SafeMath for uint;
   }
 
   // Maps each member's address to the member struct
-  mapping(address => Member) public members;
+  mapping(address => Member) internal members;
 
-  uint internal memberCount; // Stores number of members
+  uint256 internal memberCount; // Stores number of members
 
   modifier onlyExistingMember {
-    // Check if member exists or terminate
+    // Check if member exists
 
     require(bytes(members[msg.sender].name).length != 0);
     _;
   }
 
   modifier onlyValidName(string name) {
-    // Only valid names allowed
+    // Check if name is empty
 
     require(bytes(name).length != 0);
     _;
   }
 
+  /// @notice Login a member
+  /// @return the member's name 
   function login() view public onlyExistingMember returns (string) {
     return (members[msg.sender].name);
   }
 
+  /// @notice Sign up a new member
+  /// @param  name name of the member
+  /// @return member name if member exists or after a member is created
   function signup(string name) public onlyValidName(name) returns (string) {
-    // Check if member exists.
-    // If yes, return member name.
-    // If no, check if name was sent.
-    // If yes, create and return member name.
-
-    if (bytes(members[msg.sender].name).length != 0) {
+    
+    if (bytes(members[msg.sender].name).length == 0) {
         members[msg.sender].name = name;
-        memberCount.add(1);
+        memberCount++;
         return (members[msg.sender].name);
     }
     return (members[msg.sender].name);
   }
 
-  /// @notice only existing user addresses can change their name
+  /// @notice Update member's name
   /// @param  name new name of the member to change to
   function update(string name) public onlyValidName(name) onlyExistingMember returns (string) {
 
--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -5,13 +5,14 @@ import "./Authentication.sol";
 /// @title A distributed organization with members, tasks, and voting.
 /// @author Dennis Liu
 contract DAO is Authentication {
+using SafeMath for uint256;
 
     // type for a single task
     struct Task {
         address proposer; // member who proposed the task 
         string title;  // task name
         string content; // task detail
-        uint voteCount; // number of accumulated votes
+        uint256 voteCount; // number of accumulated votes
         bool nonconsensus; // bool to signal that someone voted no
         bool finished; // bool to signal voting has finished
     }
@@ -20,21 +21,21 @@ contract DAO is Authentication {
     Task[] public tasks;
 
     // Lists each task's voted members
-    mapping(uint => mapping (address => bool)) public votedMembers;
-
+    mapping(uint => mapping (address => bool)) public votedMap;
+    mapping(uint => address[]) private votedList;
 
     modifier taskVotable(uint taskId) {
         // Check if the task can be voted on
 
         require(!tasks[taskId].finished, "Voting on this task has finished");
-        require(!votedMembers[taskId][msg.sender], "Member has already voted on this task");
+        require(!votedMap[taskId][msg.sender], "Member has already voted on this task");
         _;
     }
 
     modifier taskExists(uint taskId) {
         // Check if the task exists
 
-        require(tasks[taskId].voteCount != 0, "Task does not exist");
+        require(bytes(tasks[taskId].title).length != 0, "Task does not exist");
         _;
     }
 
@@ -51,12 +52,13 @@ contract DAO is Authentication {
     event taskRemoved(uint taskId);
 
     /// @notice Propose a new task
-    /// @notice Only existing user addresses can propose a task, proposers must vote for their tasks
+    /// @notice Only existing user addresses can propose a task. proposer doesn't have to vote yes for their tasks
     /// @param  title title of the proposed task
     /// @param  content content of the proposed task
     function propose(string title, string content) public onlyExistingMember {
-        uint taskId = tasks.push(Task(msg.sender, title, content, 1, false, false));
-        votedMembers[taskId][msg.sender] = true;
+        
+        // push returns new length, hence the new task's ID is length - 1 
+        uint taskId = tasks.push(Task(msg.sender, title, content, 0, false, false)) - 1;
         emit newTask(taskId, msg.sender, title, content, 1);
     }
 
@@ -70,8 +72,9 @@ contract DAO is Authentication {
         if (!agree) {
             tasks[taskId].nonconsensus = true;
         }
-        tasks[taskId].voteCount.add(1);
-        votedMembers[taskId][msg.sender] = true;
+        tasks[taskId].voteCount++;
+        votedMap[taskId][msg.sender] = true;
+        votedList[taskId].push(msg.sender);
         emit newVote(taskId, tasks[taskId].voteCount);
         
         // set task as finished emit voteFinished event when all members have voted
@@ -87,5 +90,14 @@ contract DAO is Authentication {
     function remove(uint taskId) public onlyExistingMember taskExists(taskId) onlyProposer(taskId) {
         delete tasks[taskId];
         emit taskRemoved(taskId);
+    }
+    
+    /// @notice Return voted members for a task after its voting is finished
+    /// @notice This prevents member's votes being influenced by existing votes
+    /// @param taskId index of the task in tasks array
+    /// @return voted member addresses in an array
+    function getVoted(uint taskId) public view taskExists(taskId) onlyExistingMember returns (address[]) {
+        require(tasks[taskId].finished, "Voting on this task has not finished");
+        return votedList[taskId];
     }
 }
